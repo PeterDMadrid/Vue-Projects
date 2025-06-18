5d182a01e2f0e7bc9fd79d4c15141c7d8454ef50
Peter Madrid <petermadrid0421@gmail.com>
2025-06-18 12:12:39 +0800

    complete ver

--- src/components/selection/projects/noobCalculator.vue
+++ src/components/selection/projects/noobCalculator.vue
@@ -1,36 +1,61 @@
+<script setup>
+import { ref, computed } from 'vue'
+const buttons = [
+    ['1', '2', '3', '/'],
+    ['4', '5', '6', '*'],
+    ['7', '8', '9', '+'],
+    ['0', '.', '=', '-'],
+]
+
+const display = ref('0')
+
+const toDisplay = (value) => {
+    if (display.value === '0' && value !== '.') {
+        display.value = value
+    } else {
+        display.value = display.value + value
+    }
+}
+
+const calculate = () => {
+    try {
+        display.value = eval(display.value).toString()
+    } catch {
+        display.value = 'Error'
+    }
+}
+
+const clear = () => {
+    display.value = '0'
+}
+
+
+const handleClick = (button) => {
+    if (button === '=') {
+        calculate()
+    } else {
+        toDisplay(button)
+    }
+}
+</script>
+
 <template>
     <div class="p-4 bg-blue-50 rounded">
         <h1 class="text-2xl font-bold text-blue-800">Noob Calculator</h1><br>
         <hr>
         <div class="mt-4 h-64 p-4 border">
             <div class="flex flex-row">
-                <div class="flex items-center justify-end border mr-2 flex-[3] bg-white"></div>
-                <button
+                <input v-model="display" class="flex items-center justify-end border mr-2 flex-[3] bg-white pl-4"
+                    readonly />
+                <button @click="clear"
                     class="flex justify-center items-center border flex-1 bg-black text-white cursor-pointer">C</button>
             </div>
-            <div class="mt-2 flex">
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">1</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">2</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">3</button>
-                <button class="border p-2 flex-1 bg-black text-white cursor-pointer">/</button>
-            </div>
-            <div class="mt-2 flex">
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">4</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">5</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">6</button>
-                <button class="border p-2 flex-1 bg-black text-white cursor-pointer">*</button>
-            </div>
-            <div class="mt-2 flex">
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">7</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">8</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">9</button>
-                <button class="border p-2 flex-1 bg-black text-white cursor-pointer">+</button>
-            </div>
-            <div class="mt-2 flex">
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">0</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">.</button>
-                <button class="border p-2 flex-1 mr-2 bg-black text-white cursor-pointer">=</button>
-                <button class="border p-2 flex-1 bg-black text-white cursor-pointer">-</button>
+            <div v-for="(row, rowIndex) in buttons" :key="rowIndex" class="mt-2 flex">
+                <button v-for="(button, buttonIndex) in row" :key="buttonIndex"
+                    :class="buttonIndex < row.length - 1 ? 'mr-2' : ''" @click="handleClick(button)"
+                    class="border p-2 flex-1 bg-black text-white cursor-pointer">
+                    {{ button }}
+                </button>
             </div>
         </div>
     </div>
